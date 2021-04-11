1254cde7a28439c841c66b06f2da156f8dc6bc18
Jeppe Dybdal Larsen <au616966@uni.au.dk>
2021-04-11 12:26:13 +0200

    Removed dev dependency

--- node-reservation-sender/src/routes/hotel.ts
+++ node-reservation-sender/src/routes/hotel.ts
@@ -3,9 +3,6 @@ import * as hotelController from "../controllers/hotelController";
 
 const router = express.Router();
 
-// router.route('/:hotelId/room/:roomNumber')
-//     .get(authorize([claims.USER]), hotelController.getRoom)
-//     .put(authorize([claims.USER]), hotelController.reserveRoom);
 router.route('/:hotelId/room/:roomNumber')
     .get(hotelController.getRoom)
     .put(hotelController.reserveRoom);
